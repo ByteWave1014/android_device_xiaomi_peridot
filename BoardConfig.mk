1e1f6fad993d65d945af813cccf595d0f5ea9482
Lunark :3 <jvillaltamatos3007@gmail.com>
2025-09-16 20:31:41 -0500

    peridot: Build Npx driver - Required for NFC
    Signed-off-by: Lunark :3 <jvillaltamatos3007@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,7 +158,8 @@ TARGET_KERNEL_EXT_MODULES := \
     qcom/opensource/dsp-kernel \
     qcom/opensource/eva-kernel \
     qcom/opensource/wlan/platform \
-    qcom/opensource/wlan/qcacld-3.0
+    qcom/opensource/wlan/qcacld-3.0 \
+    nxp/opensource/driver
 
 BOOT_KERNEL_MODULES := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.vendor_dlkm))
